d276fc78a22c923e27421dd14b65136f00ad22bf
Dion Dokter <diondokter@gmail.com>
2023-11-29 21:59:18 +0100

    Remove generic_const_exprs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,4 @@
 #![cfg_attr(not(test), no_std)]
-#![allow(incomplete_features)]
-#![feature(generic_const_exprs)]
 
 use core::{
     convert::{TryFrom, TryInto},
@@ -14,25 +12,29 @@ pub trait Device {
     type Error;
     type AddressType;
 
-    fn write<R>(&mut self, data: &BitArray<[u8; size_bytes::<R>()]>) -> Result<(), Self::Error>
+    fn write<R, const SIZE_BYTES: usize>(
+        &mut self,
+        data: &BitArray<[u8; SIZE_BYTES]>,
+    ) -> Result<(), Self::Error>
     where
-        R: Register<AddressType = Self::AddressType>;
-    fn read<R>(&mut self, data: &mut BitArray<[u8; size_bytes::<R>()]>) -> Result<(), Self::Error>
+        R: Register<SIZE_BYTES, AddressType = Self::AddressType>;
+    fn read<R, const SIZE_BYTES: usize>(
+        &mut self,
+        data: &mut BitArray<[u8; SIZE_BYTES]>,
+    ) -> Result<(), Self::Error>
     where
-        R: Register<AddressType = Self::AddressType>;
+        R: Register<SIZE_BYTES, AddressType = Self::AddressType>;
 }
 
-pub trait Register {
+pub trait Register<const SIZE_BYTES: usize> {
     const ZERO: Self;
 
     type AddressType;
     const ADDRESS: Self::AddressType;
 
-    const SIZE_BITS: usize;
-
     type RWCapability;
 
-    fn bits(&mut self) -> &mut BitArray<[u8; size_bytes::<Self>()]>;
+    fn bits(&mut self) -> &mut BitArray<[u8; SIZE_BYTES]>;
 
     fn default() -> Self
     where
@@ -42,23 +44,15 @@ pub trait Register {
     }
 }
 
-pub const fn size_bytes<R: Register + ?Sized>() -> usize {
-    if R::SIZE_BITS % 8 == 0 {
-        R::SIZE_BITS / 8
-    } else {
-        R::SIZE_BITS / 8 + 1
-    }
-}
-
-pub struct RegisterOperation<'a, D: Device, R: Register> {
+pub struct RegisterOperation<'a, D: Device, R: Register<SIZE_BYTES>, const SIZE_BYTES: usize> {
     device: &'a mut D,
     _phantom: PhantomData<R>,
 }
 
-impl<'a, D, R> RegisterOperation<'a, D, R>
+impl<'a, D, R, const SIZE_BYTES: usize> RegisterOperation<'a, D, R, SIZE_BYTES>
 where
     D: Device<AddressType = R::AddressType>,
-    R: Register,
+    R: Register<SIZE_BYTES>,
 {
     pub fn new(device: &'a mut D) -> Self {
         Self {
@@ -68,61 +62,67 @@ where
     }
 }
 
-impl<'a, D, R> RegisterOperation<'a, D, R>
+impl<'a, D, R, const SIZE_BYTES: usize> RegisterOperation<'a, D, R, SIZE_BYTES>
 where
     D: Device<AddressType = R::AddressType>,
-    R: Register,
+    R: Register<SIZE_BYTES>,
     R::RWCapability: WriteCapability,
 {
-    pub fn write(&mut self, f: impl FnOnce(&mut R) -> &mut R) -> Result<(), D::Error>
-    where
-        [(); size_bytes::<R>()]:,
-    {
+    pub fn write(&mut self, f: impl FnOnce(&mut R) -> &mut R) -> Result<(), D::Error> {
         let mut register = R::ZERO;
         f(&mut register);
-        self.device.write::<R>(register.bits())
+        self.device.write::<R, SIZE_BYTES>(register.bits())
     }
 }
 
-impl<'a, D, R> RegisterOperation<'a, D, R>
+impl<'a, D, R, const SIZE_BYTES: usize> RegisterOperation<'a, D, R, SIZE_BYTES>
 where
     D: Device<AddressType = R::AddressType>,
-    R: Register,
+    R: Register<SIZE_BYTES>,
     R::RWCapability: ReadCapability,
 {
-    pub fn read(&mut self) -> Result<R, D::Error>
-    where
-        [(); size_bytes::<R>()]:,
-    {
+    pub fn read(&mut self) -> Result<R, D::Error> {
         let mut register = R::ZERO;
-        self.device.read::<R>(register.bits())?;
+        self.device.read::<R, SIZE_BYTES>(register.bits())?;
         Ok(register)
     }
 }
 
-impl<'a, D, R> RegisterOperation<'a, D, R>
+impl<'a, D, R, const SIZE_BYTES: usize> RegisterOperation<'a, D, R, SIZE_BYTES>
 where
     D: Device<AddressType = R::AddressType>,
-    R: Register,
+    R: Register<SIZE_BYTES>,
     R::RWCapability: ReadCapability + WriteCapability,
 {
-    pub fn modify(&mut self, f: impl FnOnce(&mut R) -> &mut R) -> Result<(), D::Error>
-    where
-        [(); size_bytes::<R>()]:,
-    {
+    pub fn modify(&mut self, f: impl FnOnce(&mut R) -> &mut R) -> Result<(), D::Error> {
         let mut register = self.read()?;
         f(&mut register);
-        self.device.write::<R>(register.bits())
+        self.device.write::<R, SIZE_BYTES>(register.bits())
     }
 }
 
-pub struct Field<'a, R: Register, DATA, BACKING, const START: usize, const END: usize> {
+pub struct Field<
+    'a,
+    R: Register<SIZE_BYTES>,
+    DATA,
+    BACKING,
+    const START: usize,
+    const END: usize,
+    const SIZE_BYTES: usize,
+> {
     register: &'a mut R,
     _phantom: PhantomData<(DATA, BACKING)>,
 }
 
-impl<'a, R: Register, DATA, BACKING, const START: usize, const END: usize>
-    Field<'a, R, DATA, BACKING, START, END>
+impl<
+        'a,
+        R: Register<SIZE_BYTES>,
+        DATA,
+        BACKING,
+        const START: usize,
+        const END: usize,
+        const SIZE_BYTES: usize,
+    > Field<'a, R, DATA, BACKING, START, END, SIZE_BYTES>
 where
     DATA: TryFrom<BACKING> + Into<BACKING>,
     BACKING: Integral,
@@ -134,18 +134,12 @@ where
         }
     }
 
-    pub fn set(self, data: DATA) -> &'a mut R
-    where
-        [(); size_bytes::<R>()]:,
-    {
+    pub fn set(self, data: DATA) -> &'a mut R {
         self.register.bits()[START..END].store_be(data.into());
         self.register
     }
 
-    pub fn get(self) -> Result<DATA, <BACKING as TryInto<DATA>>::Error>
-    where
-        [(); size_bytes::<R>()]:,
-    {
+    pub fn get(self) -> Result<DATA, <BACKING as TryInto<DATA>>::Error> {
         self.register.bits()[START..END]
             .load_be::<BACKING>()
             .try_into()
@@ -176,25 +170,27 @@ pub mod tests {
         type Error = ();
         type AddressType = usize;
 
-        fn write<R>(&mut self, data: &BitArray<[u8; size_bytes::<R>()]>) -> Result<(), Self::Error>
+        fn write<R, const SIZE_BYTES: usize>(
+            &mut self,
+            data: &BitArray<[u8; SIZE_BYTES]>,
+        ) -> Result<(), Self::Error>
         where
-            R: Register<AddressType = Self::AddressType>,
+            R: Register<SIZE_BYTES, AddressType = Self::AddressType>,
         {
-            self.device_memory[R::ADDRESS..][..size_bytes::<R>()]
-                .copy_from_slice(data.as_raw_slice());
+            self.device_memory[R::ADDRESS..][..SIZE_BYTES].copy_from_slice(data.as_raw_slice());
 
             Ok(())
         }
 
-        fn read<R>(
+        fn read<R, const SIZE_BYTES: usize>(
             &mut self,
-            data: &mut BitArray<[u8; size_bytes::<R>()]>,
+            data: &mut BitArray<[u8; SIZE_BYTES]>,
         ) -> Result<(), Self::Error>
         where
-            R: Register<AddressType = Self::AddressType>,
+            R: Register<SIZE_BYTES, AddressType = Self::AddressType>,
         {
             data.as_raw_mut_slice()
-                .copy_from_slice(&self.device_memory[R::ADDRESS..][..size_bytes::<R>()]);
+                .copy_from_slice(&self.device_memory[R::ADDRESS..][..SIZE_BYTES]);
             Ok(())
         }
     }
@@ -207,21 +203,25 @@ pub mod tests {
             }
         }
 
-        pub fn device_id(&mut self) -> RegisterOperation<'_, Self, DeviceId> {
+        pub fn device_id(
+            &mut self,
+        ) -> RegisterOperation<'_, Self, DeviceId, { DeviceId::SIZE_BYTES }> {
             RegisterOperation::new(self)
         }
     }
 
     struct DeviceId {
-        bits: BitArray<[u8; size_bytes::<Self>()]>,
+        bits: BitArray<[u8; Self::SIZE_BYTES]>,
     }
 
     impl DeviceId {
-        pub fn manufacturer(&mut self) -> Field<'_, Self, u8, u8, 0, 8> {
+        pub const SIZE_BYTES: usize = 2;
+
+        pub fn manufacturer(&mut self) -> Field<'_, Self, u8, u8, 0, 8, { Self::SIZE_BYTES }> {
             Field::new(self)
         }
 
-        pub fn series(&mut self) -> Field<'_, Self, Series, u8, 8, 12> {
+        pub fn series(&mut self) -> Field<'_, Self, Series, u8, 8, 12, { Self::SIZE_BYTES }> {
             Field::new(self)
         }
     }
@@ -234,7 +234,7 @@ pub mod tests {
         C,
     }
 
-    impl Register for DeviceId {
+    impl Register<2> for DeviceId {
         const ZERO: Self = Self {
             bits: BitArray::ZERO,
         };
@@ -242,11 +242,9 @@ pub mod tests {
         type AddressType = usize;
         const ADDRESS: Self::AddressType = 0;
 
-        const SIZE_BITS: usize = 12;
-
         type RWCapability = ReadWrite;
 
-        fn bits(&mut self) -> &mut BitArray<[u8; size_bytes::<Self>()]> {
+        fn bits(&mut self) -> &mut BitArray<[u8; 2]> {
             &mut self.bits
         }
     }
