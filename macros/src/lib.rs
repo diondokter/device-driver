b4579bae8eec9e1695d1b1588a27ee9f5ca702aa
Dion Dokter <diondokter@gmail.com>
2024-09-09 13:20:12 +0200

    Recreate macro

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,16 +1,86 @@
 #![doc = include_str!(concat!("../", env!("CARGO_PKG_README")))]
 
 use proc_macro::TokenStream;
-
-mod from_file;
-mod from_macro;
-
-#[proc_macro_attribute]
-pub fn implement_device_from_file(attr: TokenStream, item: TokenStream) -> TokenStream {
-    from_file::implement_device_from_file(attr, item)
-}
+use syn::{braced, Ident, LitStr};
 
 #[proc_macro]
 pub fn implement_device(item: TokenStream) -> TokenStream {
-    from_macro::implement_device(item.into()).into()
+    let input = match syn::parse::<Input>(item) {
+        Ok(i) => i,
+        Err(e) => return e.into_compile_error().into(),
+    };
+
+    match input.generation_type {
+        GenerationType::Dsl(tokens) => {
+            device_driver_generation::transform_dsl(tokens, &input.device_name.to_string()).into()
+        }
+        GenerationType::Json(_) => todo!(),
+        GenerationType::Yaml(_) => todo!(),
+    }
+}
+
+struct Input {
+    device_name: Ident,
+    generation_type: GenerationType,
+}
+
+enum GenerationType {
+    Dsl(proc_macro2::TokenStream),
+    Json(LitStr),
+    Yaml(LitStr),
+}
+
+impl syn::parse::Parse for Input {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        input.parse::<kw::name>()?;
+        input.parse::<syn::Token![:]>()?;
+        let device_name = input.parse()?;
+        input.parse::<syn::Token![,]>()?;
+
+        let look = input.lookahead1();
+
+        if look.peek(kw::dsl) {
+            input.parse::<kw::dsl>()?;
+            input.parse::<syn::Token![:]>()?;
+
+            let braced;
+            braced!(braced in input);
+
+            let tokens = braced.parse()?;
+
+            Ok(Self {
+                device_name,
+                generation_type: GenerationType::Dsl(tokens),
+            })
+        } else if look.peek(kw::json) {
+            input.parse::<kw::json>()?;
+            input.parse::<syn::Token![:]>()?;
+
+            let path = input.parse()?;
+
+            Ok(Self {
+                device_name,
+                generation_type: GenerationType::Json(path),
+            })
+        } else if look.peek(kw::yaml) {
+            input.parse::<kw::yaml>()?;
+            input.parse::<syn::Token![:]>()?;
+
+            let path = input.parse()?;
+
+            Ok(Self {
+                device_name,
+                generation_type: GenerationType::Yaml(path),
+            })
+        } else {
+            Err(look.error())
+        }
+    }
+}
+
+mod kw {
+    syn::custom_keyword!(name);
+    syn::custom_keyword!(dsl);
+    syn::custom_keyword!(json);
+    syn::custom_keyword!(yaml);
 }
