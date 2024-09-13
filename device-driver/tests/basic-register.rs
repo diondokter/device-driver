a2dd4cf762e4610951bdc6f18d9b669ffc12704a
Dion Dokter <dion@tweedegolf.com>
2024-09-13 16:37:46 +0200

    Add more integration tests

--- device-driver/tests/basic-register.rs
+++ device-driver/tests/basic-register.rs
@@ -55,6 +55,20 @@ pub mod registers {
                 value0: bool = 0..1,
                 value1: uint = 1..16,
                 value2: int = 16..24,
+            },
+            /// This is the Foo register
+            register FooRepeated {
+                const ADDRESS = 3;
+                const SIZE_BITS = 24;
+                const REPEAT = {
+                    count: 4,
+                    stride: 3,
+                };
+
+                /// This is a bool!
+                value0: bool = 0..1,
+                value1: uint = 1..16,
+                value2: int = 16..24,
             }
         }
     );
@@ -67,7 +81,7 @@ pub mod registers {
         let reg = device.foo().read().unwrap();
 
         assert_eq!(reg.value_0(), false);
-        assert_eq!(reg.value_1(), 12345u16);
+        assert_eq!(reg.value_1(), 12345);
         assert_eq!(reg.value_2(), 0i8);
 
         device
@@ -78,9 +92,33 @@ pub mod registers {
         let reg = device.foo().read().unwrap();
 
         assert_eq!(reg.value_0(), true);
-        assert_eq!(reg.value_1(), 12345u16);
+        assert_eq!(reg.value_1(), 12345);
         assert_eq!(reg.value_2(), -1);
 
-        // device.interface.device_memory
+        assert_eq!(
+            &device.interface.device_memory[0..3],
+            &[(0x39 << 1) + 1, 0x30 << 1, 0xFF]
+        );
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_repeated_too_large_index() {
+        let mut device = MyTestDevice::new(DeviceInterface::new());
+        device.foo_repeated(4);
+    }
+
+    #[test]
+    fn test_repeated_read_modify_write() {
+        let mut device = MyTestDevice::new(DeviceInterface::new());
+        device
+            .foo_repeated(2)
+            .modify(|w| w.set_value_0(true).set_value_1(12345).set_value_2(-1))
+            .unwrap();
+
+        assert_eq!(
+            &device.interface.device_memory[9..12],
+            &[(0x39 << 1) + 1, 0x30 << 1, 0xFF]
+        );
     }
 }
---
+++ device-driver/tests/endianness-respected.rs
@@ -0,0 +1,112 @@
+use device_driver::RegisterInterface;
+
+pub struct DeviceInterface;
+
+impl RegisterInterface for DeviceInterface {
+    type Error = ();
+    type AddressType = u8;
+
+    #[track_caller]
+    fn write_register(
+        &mut self,
+        _address: Self::AddressType,
+        data: &[u8],
+    ) -> Result<(), Self::Error> {
+        // Assert data is little endian
+        assert_eq!(data, &[0x34, 0x12]);
+
+        Ok(())
+    }
+
+    fn read_register(
+        &mut self,
+        _address: Self::AddressType,
+        data: &mut [u8],
+    ) -> Result<(), Self::Error> {
+        data.copy_from_slice(&[0x32, 0x12]);
+        Ok(())
+    }
+}
+
+pub mod registers {
+    use super::*;
+
+    device_driver_macros::implement_device!(
+        device_name: MyTestDevice,
+        dsl: {
+            config {
+                type RegisterAddressType = u8;
+            }
+            register FooLE {
+                const ADDRESS = 0;
+                const SIZE_BITS = 16;
+                type ByteOrder = LE;
+                const ALLOW_ADDRESS_OVERLAP = true;
+                const RESET_VALUE = 0x1234;
+
+                val: uint = 0..16,
+            },
+            register FooLEArray {
+                const ADDRESS = 0;
+                const SIZE_BITS = 16;
+                type ByteOrder = LE;
+                const ALLOW_ADDRESS_OVERLAP = true;
+                const RESET_VALUE = [0x34, 0x12]; // As the read and write functions see it
+
+                val: uint = 0..16,
+            },
+            register FooBE {
+                const ADDRESS = 0;
+                const SIZE_BITS = 16;
+                type ByteOrder = BE;
+                const ALLOW_ADDRESS_OVERLAP = true;
+                const RESET_VALUE = 0x3412;
+
+                val: uint = 0..16,
+            },
+            register FooBEArray {
+                const ADDRESS = 0;
+                const SIZE_BITS = 16;
+                type ByteOrder = BE;
+                const ALLOW_ADDRESS_OVERLAP = true;
+                const RESET_VALUE = [0x34, 0x12];
+
+                val: uint = 0..16,
+            }
+        }
+    );
+
+    #[test]
+    fn little_endian_respected() {
+        let mut device = MyTestDevice::new(DeviceInterface);
+
+        device.foo_le().write(|w| w).unwrap();
+        device
+            .foo_le()
+            .write_with_zero(|w| w.set_val(0x1234))
+            .unwrap();
+
+        device.foo_le_array().write(|w| w).unwrap();
+        device
+            .foo_le_array()
+            .write_with_zero(|w| w.set_val(0x1234))
+            .unwrap();
+    }
+
+    #[test]
+    fn big_endian_respected() {
+        let mut device = MyTestDevice::new(DeviceInterface);
+
+        device.foo_be().write(|w| w).unwrap();
+        device
+            .foo_be()
+            .write_with_zero(|w| w.set_val(0x3412))
+            .unwrap();
+
+        device.foo_be_array().write(|w| w).unwrap();
+        device
+            .foo_be_array()
+            .write_with_zero(|w| w.set_val(0x3412))
+            .unwrap();
+    }
+}
