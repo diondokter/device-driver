01ebc3de9222c5992e2deed56e8f070105efca95
Dion Dokter <diondokter@gmail.com>
2025-02-05 17:39:15 +0100

    A working load_msb0!

--- device-driver/src/ops.rs
+++ device-driver/src/ops.rs
@@ -1,11 +1,8 @@
-use core::ops::{BitAnd, BitOrAssign, Shl, Shr};
+use core::ops::{BitOrAssign, Shl};
 
 pub unsafe fn load_lsb0<T, ByteO: ByteOrder>(data: &[u8], start: usize, end: usize) -> T
 where
-    T: Default
-        + From<u8>
-        + Shl<usize, Output = T>
-        + BitOrAssign,
+    T: Default + From<u8> + Shl<usize, Output = T> + BitOrAssign,
 {
     let mut output = T::default();
 
@@ -28,17 +25,50 @@ where
 
 pub unsafe fn load_msb0<T, ByteO: ByteOrder>(data: &[u8], start: usize, end: usize) -> T
 where
-    T: Default
-        + From<u8>
-        + Shr<usize, Output = T>
-        + BitAnd<T, Output = T>
-        + Shl<usize, Output = T>
-        + BitOrAssign
-        + SwapBytes,
+    T: Default + From<u8> + Shl<usize, Output = T> + BitOrAssign,
 {
-    let new_start = data.len() * 8 - end;
-    let new_end = data.len() * 8 - start;
-    load_lsb0::<T, ByteO::Opposite>(data, new_start, new_end)//.swap_bytes()
+    let mut output = T::default();
+
+    let mut i = start;
+    while i < end {
+        let byte = ByteO::get_byte_from_index(data, i);
+
+        if (i % 8 == 0) & (i + 8 <= end) {
+            output |= T::from(byte) << (i - start);
+            i += 8;
+        } else {
+            // Get the bit MSB0 (so reversed)
+            let bit = (byte >> (7 - i % 8)) & 1;
+
+            // We still need to store in normal order, so we need to reverse again, but within the index
+            let (num_mirror_bits, pivot) = if i / 8 == start / 8 {
+                // We are in the start byte
+                let num_mirror_bits = (start + 1).next_multiple_of(8).min(end) - start;
+                let pivot = start + num_mirror_bits / 2;
+
+                (num_mirror_bits, pivot)
+            } else {
+                // We are in the end byte
+                let num_mirror_bits = end - (end - 8).next_multiple_of(8);
+                let pivot = end - ((num_mirror_bits + 1) / 2);
+
+                (num_mirror_bits, pivot)
+            };
+
+            let mut diff = pivot as isize - i as isize;
+
+            if diff <= 0 && num_mirror_bits % 2 == 0 {
+                diff -= 1;
+            }
+
+            let j = i as isize + diff * 2 - ((num_mirror_bits % 2 == 0) as isize) * diff.signum();
+
+            output |= T::from(bit) << (j as usize - start);
+            i += 1;
+        }
+    }
+
+    output
 }
 
 pub struct LE;
@@ -46,55 +76,31 @@ pub struct BE;
 
 pub trait ByteOrder {
     type Opposite: ByteOrder;
-    unsafe fn get_byte_from_index(data: &[u8], bit_index: usize) -> u8;
+
+    fn get_byte_index(data_len: usize, bit_index: usize) -> usize;
+    unsafe fn get_byte_from_index(data: &[u8], bit_index: usize) -> u8 {
+        *data.get_unchecked(Self::get_byte_index(data.len(), bit_index))
+    }
 }
 
 impl ByteOrder for LE {
     type Opposite = BE;
 
-    unsafe fn get_byte_from_index(data: &[u8], bit_index: usize) -> u8 {
-        *data.get_unchecked(bit_index / 8)
+    fn get_byte_index(_data_len: usize, bit_index: usize) -> usize {
+        bit_index / 8
     }
 }
 
 impl ByteOrder for BE {
     type Opposite = LE;
 
-    unsafe fn get_byte_from_index(data: &[u8], bit_index: usize) -> u8 {
-        *data.get_unchecked(data.len() - (bit_index / 8) - 1)
+    fn get_byte_index(data_len: usize, bit_index: usize) -> usize {
+        data_len - (bit_index / 8) - 1
     }
 }
 
-pub trait SwapBytes {
-    fn swap_bytes(self) -> Self;
-}
-
-macro_rules! impl_swap_bytes {
-    ($integer:ty) => {
-        impl SwapBytes for $integer {
-            fn swap_bytes(self) -> Self {
-                <$integer>::swap_bytes(self)
-            }
-        }
-    };
-}
-
-impl_swap_bytes!(u8);
-impl_swap_bytes!(u16);
-impl_swap_bytes!(u32);
-impl_swap_bytes!(u64);
-impl_swap_bytes!(u128);
-
-impl_swap_bytes!(i8);
-impl_swap_bytes!(i16);
-impl_swap_bytes!(i32);
-impl_swap_bytes!(i64);
-impl_swap_bytes!(i128);
-
 #[cfg(test)]
 mod tests {
-    use bitvec::view::BitView;
-
     use super::*;
 
     #[test]
@@ -151,8 +157,8 @@ mod tests {
     fn same_as_bitvec() {
         use bitvec::{field::BitField, view::BitView};
 
-        for _ in 0..1_000 {
-            let mut data = vec![0u8; rand::random_range(2..=2)];
+        for _ in 0..1_000_000 {
+            let mut data = vec![0u8; rand::random_range(1..=16)];
             rand::fill(&mut data[..]);
             let mut reversed_data = data.clone();
             reversed_data.reverse();
@@ -164,32 +170,30 @@ mod tests {
 
             println!("{start}..{end} @ {:#010b}", Bytes(&data));
 
-            // let test_value = unsafe { load_lsb0::<u32, LE>(&data, start, end) };
-            // let check_value = data.view_bits::<bitvec::order::Lsb0>()[start..end].load_le::<u32>();
-            // println!("LE Lsb0: {:08b} *", Bytes(&check_value.to_be_bytes()));
-            // println!("LE Lsb0: {:08b}", Bytes(&test_value.to_be_bytes()));
-            // assert_eq!(test_value, check_value);
+            let test_value = unsafe { load_lsb0::<u32, LE>(&data, start, end) };
+            let check_value = data.view_bits::<bitvec::order::Lsb0>()[start..end].load_le::<u32>();
+            println!("LE Lsb0: {:016b} *", check_value);
+            println!("LE Lsb0: {:016b}", test_value);
+            assert_eq!(test_value, check_value);
 
-            // let test_value = unsafe { load_lsb0::<u32, BE>(&data, start, end) };
-            // let check_value =
-            //     reversed_data.view_bits::<bitvec::order::Lsb0>()[start..end].load_le::<u32>();
-            // println!("BE Lsb0: {:08b} *", Bytes(&check_value.to_be_bytes()));
-            // println!("BE Lsb0: {:08b}", Bytes(&test_value.to_be_bytes()));
-            // assert_eq!(test_value, check_value);
+            let test_value = unsafe { load_lsb0::<u32, BE>(&data, start, end) };
+            let check_value =
+                reversed_data.view_bits::<bitvec::order::Lsb0>()[start..end].load_le::<u32>();
+            println!("BE Lsb0: {:016b} *", check_value);
+            println!("BE Lsb0: {:016b}", test_value);
+            assert_eq!(test_value, check_value);
 
             let test_value_le_msb0 = unsafe { load_msb0::<u32, LE>(&data, start, end) };
-            let check_value_le_msb0 = data.view_bits::<bitvec::order::Msb0>()[start..end].load_le::<u32>();
-            println!("LE Msb0: {:08b} *", Bytes(&check_value_le_msb0.to_be_bytes()));
-            println!("LE Msb0: {:08b}", Bytes(&test_value_le_msb0.to_be_bytes()));
+            let check_value_le_msb0 =
+                data.view_bits::<bitvec::order::Msb0>()[start..end].load_le::<u32>();
+            println!("LE Msb0: {:016b} *", check_value_le_msb0);
+            println!("LE Msb0: {:016b}", test_value_le_msb0);
 
             let test_value_be_msb0 = unsafe { load_msb0::<u32, BE>(&data, start, end) };
             let check_value_be_msb0 =
                 reversed_data.view_bits::<bitvec::order::Msb0>()[start..end].load_le::<u32>();
-            println!(
-                "BE Msb0: {:08b} *",
-                Bytes(&check_value_be_msb0.to_be_bytes())
-            );
-            println!("BE Msb0: {:08b}", Bytes(&test_value_be_msb0.to_be_bytes()));
+            println!("BE Msb0: {:016b} *", check_value_be_msb0);
+            println!("BE Msb0: {:016b}", test_value_be_msb0);
 
             assert_eq!(test_value_le_msb0, check_value_le_msb0);
             assert_eq!(test_value_be_msb0, check_value_be_msb0);
