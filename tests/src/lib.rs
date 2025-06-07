f6547983eda2a8119cdd864bafdda4d180530f8d
Dion Dokter <diondokter@gmail.com>
2025-06-07 16:09:04 +0200

    Improve error messages and add readme

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -20,7 +20,12 @@ pub fn run_test(input_path: &Path, output_path: &Path) {
 
     let transformed =
         OUTPUT_HEADER.to_string() + &device_driver_generation::transform_yaml(&input, "Device");
-    pretty_assertions::assert_str_eq!(output, transformed);
+    pretty_assertions::assert_str_eq!(
+        output,
+        transformed,
+        "Failed output file: {}",
+        output_path.display()
+    );
 
     let output_extension = output_path.extension().unwrap().to_str().unwrap();
     match output_extension {
@@ -42,6 +47,10 @@ fn compile_output(output_path: &Path) {
     let output = cmd.output().unwrap();
 
     if !output.status.success() {
-        panic!("{}", String::from_utf8(output.stderr).unwrap());
+        panic!(
+            "Could not compile output file {}:\n{}",
+            output_path.display(),
+            String::from_utf8(output.stderr).unwrap()
+        );
     }
 }
