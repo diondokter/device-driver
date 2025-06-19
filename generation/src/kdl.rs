d66e47f01045ee572769e92ab1dfab08514858b2
Dion Dokter <diondokter@gmail.com>
2025-06-19 17:18:17 +0200

    Add processing of global config access options

--- generation/src/kdl.rs
+++ generation/src/kdl.rs
@@ -1,17 +1,14 @@
-use std::path::Path;
+use std::{collections::HashMap, path::Path, str::FromStr};
 
-use kdl::{KdlNode, KdlValue};
+use kdl::{KdlDocument, KdlNode, KdlValue};
+use miette::SourceSpan;
 
 use crate::{
-    mir,
+    mir::{Access, Device, GlobalConfig},
     reporting::{Diagnostics, NamedSourceCode, errors},
 };
 
-pub fn transform(
-    source: &str,
-    file_path: &Path,
-    diagnostics: &mut Diagnostics,
-) -> Vec<mir::Device> {
+pub fn transform(source: &str, file_path: &Path, diagnostics: &mut Diagnostics) -> Vec<Device> {
     let source_code = NamedSourceCode::new(file_path.display().to_string(), source.into());
 
     let document = match kdl::KdlDocument::parse(source) {
@@ -35,11 +32,12 @@ fn transform_device(
     node: &KdlNode,
     source_code: NamedSourceCode,
     diagnostics: &mut Diagnostics,
-) -> Option<mir::Device> {
+) -> Option<Device> {
     if node.name().value() != "device" {
-        diagnostics.add(errors::UnknownRootKeyword {
+        diagnostics.add(errors::UnexpectedNode {
             source_code,
-            keyword: node.name().span(),
+            node_name: node.name().span(),
+            expected_names: vec!["device"],
         });
         return None;
     }
@@ -49,7 +47,7 @@ fn transform_device(
             if let KdlValue::String(device_name) = entry.value()
                 && entry.name().is_none()
             {
-                device_name
+                device_name.clone()
             } else {
                 diagnostics.add(errors::MissingObjectName {
                     source_code,
@@ -79,5 +77,212 @@ fn transform_device(
         return None;
     }
 
-    None
+    let mut device = Device {
+        name: Some(device_name),
+        global_config: GlobalConfig::default(),
+        objects: Vec::new(),
+    };
+
+    if let Some(device_document) = node.children()
+        && !device_document.nodes().is_empty()
+    {
+        transform_device_internals(&mut device, device_document, source_code, diagnostics);
+    } else {
+        diagnostics.add(errors::EmptyNode {
+            source_code,
+            node: node.span(),
+        });
+    }
+
+    Some(device)
+}
+
+fn transform_device_internals(
+    device: &mut Device,
+    device_document: &KdlDocument,
+    source_code: NamedSourceCode,
+    diagnostics: &mut Diagnostics,
+) {
+    let mut seen_global_configs = HashMap::<GlobalConfigType, SourceSpan>::new();
+
+    for node in device_document.nodes() {
+        if let Ok(global_config_type) = node.name().value().parse::<GlobalConfigType>() {
+            match seen_global_configs.insert(global_config_type, node.span()) {
+                None => transform_global_config_node(
+                    device,
+                    node,
+                    global_config_type,
+                    source_code.clone(),
+                    diagnostics,
+                ),
+                Some(original_node) => {
+                    diagnostics.add(errors::DuplicateNode {
+                        source_code: source_code.clone(),
+                        duplicate: node.span(),
+                        original: original_node,
+                    });
+                }
+            }
+        } else if let Ok(object_type) = node.name().value().parse::<ObjectType>() {
+        } else {
+            diagnostics.add(errors::UnexpectedNode {
+                source_code: source_code.clone(),
+                node_name: node.name().span(),
+                expected_names: GLOBAL_CONFIG_TYPES
+                    .iter()
+                    .map(|v| v.0)
+                    .chain(OBJECT_TYPES.iter().map(|v| v.0))
+                    .collect(),
+            });
+        }
+    }
+}
+
+fn transform_global_config_node(
+    device: &mut Device,
+    node: &KdlNode,
+    global_config_type: GlobalConfigType,
+    source_code: NamedSourceCode,
+    diagnostics: &mut Diagnostics,
+) {
+    match global_config_type {
+        GlobalConfigType::DefaultRegisterAccess => {
+            if let Some(access) = parse_access(node, source_code.clone(), diagnostics) {
+                device.global_config.default_register_access = access;
+            }
+        }
+        GlobalConfigType::DefaultFieldAccess => {
+            if let Some(access) = parse_access(node, source_code.clone(), diagnostics) {
+                device.global_config.default_field_access = access;
+            }
+        }
+        GlobalConfigType::DefaultBufferAccess => {
+            if let Some(access) = parse_access(node, source_code.clone(), diagnostics) {
+                device.global_config.default_buffer_access = access;
+            }
+        }
+        GlobalConfigType::DefaultByteOrder => todo!(),
+        GlobalConfigType::DefaultBitOrder => todo!(),
+        GlobalConfigType::RegisterAddressType => todo!(),
+        GlobalConfigType::CommandAddressType => todo!(),
+        GlobalConfigType::BufferAddressType => todo!(),
+        GlobalConfigType::NameWordBoundaries => todo!(),
+        GlobalConfigType::DefmtFeature => todo!(),
+    }
+}
+
+fn parse_access(
+    node: &KdlNode,
+    source_code: NamedSourceCode,
+    diagnostics: &mut Diagnostics,
+) -> Option<Access> {
+    let unexpected_entries = errors::UnexpectedEntries {
+        source_code: source_code.clone(),
+        entries: node
+            .entries()
+            .iter()
+            .enumerate()
+            .filter(|(index, entry)| *index > 0 || entry.name().is_some())
+            .map(|(_, entry)| entry.span())
+            .collect(),
+    };
+    if !unexpected_entries.entries.is_empty() {
+        diagnostics.add(unexpected_entries);
+    }
+
+    match node.entries().first() {
+        Some(entry) if entry.name().is_none() => match entry.value() {
+            KdlValue::String(val) if val == "RW" => Some(Access::RW),
+            KdlValue::String(val) if val == "RO" => Some(Access::RO),
+            KdlValue::String(val) if val == "WO" => Some(Access::WO),
+            _ => {
+                diagnostics.add(errors::UnexpectedValue {
+                    source_code,
+                    value_name: entry.span(),
+                    expected_values: vec!["RW", "RO", "WO"],
+                });
+                None
+            }
+        },
+        _ => {
+            diagnostics.add(errors::MissingEntry {
+                source_code,
+                node_name: node.name().span(),
+                expected_entries: vec!["RW", "RO", "WO"],
+            });
+            None
+        }
+    }
+}
+
+#[rustfmt::skip]
+const GLOBAL_CONFIG_TYPES: &[(&str, GlobalConfigType)] = &[
+    ("default-register-access", GlobalConfigType::DefaultRegisterAccess),
+    ("default-field-access", GlobalConfigType::DefaultFieldAccess),
+    ("default-buffer-access", GlobalConfigType::DefaultBufferAccess),
+    ("default-byte-order", GlobalConfigType::DefaultByteOrder),
+    ("default-bit-order", GlobalConfigType::DefaultBitOrder),
+    ("register-address-type", GlobalConfigType::RegisterAddressType),
+    ("command-address-type", GlobalConfigType::CommandAddressType),
+    ("buffer-address-type", GlobalConfigType::BufferAddressType),
+    ("name-word-boundaries", GlobalConfigType::NameWordBoundaries),
+    ("defmt-feature", GlobalConfigType::DefmtFeature),
+];
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+enum GlobalConfigType {
+    DefaultRegisterAccess,
+    DefaultFieldAccess,
+    DefaultBufferAccess,
+    DefaultByteOrder,
+    DefaultBitOrder,
+    RegisterAddressType,
+    CommandAddressType,
+    BufferAddressType,
+    NameWordBoundaries,
+    DefmtFeature,
+}
+
+impl FromStr for GlobalConfigType {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        for (name, val) in GLOBAL_CONFIG_TYPES {
+            if *name == s {
+                return Ok(*val);
+            }
+        }
+
+        Err(())
+    }
+}
+
+#[rustfmt::skip]
+const OBJECT_TYPES: &[(&str, ObjectType)] = &[
+    ("block", ObjectType::Block),
+    ("register", ObjectType::Register),
+    ("command", ObjectType::Command),
+    ("buffer", ObjectType::Buffer),
+    ("ref", ObjectType::Ref),
+];
+#[derive(Clone, Copy)]
+enum ObjectType {
+    Block,
+    Register,
+    Command,
+    Buffer,
+    Ref,
+}
+
+impl FromStr for ObjectType {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        for (name, val) in OBJECT_TYPES {
+            if *name == s {
+                return Ok(*val);
+            }
+        }
+
+        Err(())
+    }
 }
--- generation/src/reporting/errors.rs
+++ generation/src/reporting/errors.rs
@@ -1,17 +1,8 @@
+use itertools::Itertools;
 use miette::{Diagnostic, SourceSpan};
 use thiserror::Error;
 
 use crate::reporting::NamedSourceCode;
-
-#[derive(Error, Debug, Diagnostic)]
-#[error("Unknown keyword")]
-#[diagnostic(help("Expected `device`"), severity(Error))]
-pub struct UnknownRootKeyword {
-    #[source_code]
-    pub source_code: NamedSourceCode,
-    #[label("Unknown keyword")]
-    pub keyword: SourceSpan,
-}
 
 #[derive(Error, Debug, Diagnostic)]
 #[error("Missing object name")]
@@ -34,7 +25,10 @@ pub struct MissingObjectName {
 #[derive(Error, Debug, Diagnostic)]
 #[error("Unexpected entries")]
 #[diagnostic(
-    help("Not all data is entered as entries. Check the book to see the specification"),
+    help(
+        "Some entries require a name, require to be anonymous or are expected to be of a certain type. Check the book to see the specification
+These entries may also just be superfluous. Try removing them or check other errors to see what's expected"
+    ),
     severity(Error)
 )]
 pub struct UnexpectedEntries {
@@ -42,4 +36,105 @@ pub struct UnexpectedEntries {
     pub source_code: NamedSourceCode,
     #[label(collection)]
     pub entries: Vec<SourceSpan>,
+}
+
+#[derive(Error, Debug, Diagnostic)]
+#[error("Unexpected node")]
+#[diagnostic(
+    help("Expected a node with one of these names: {}", self.print_expected_names()),
+    severity(Error)
+)]
+pub struct UnexpectedNode {
+    #[source_code]
+    pub source_code: NamedSourceCode,
+    #[label("The unexpected node")]
+    pub node_name: SourceSpan,
+
+    pub expected_names: Vec<&'static str>,
+}
+
+impl UnexpectedNode {
+    fn print_expected_names(&self) -> String {
+        self.expected_names
+            .iter()
+            .map(|name| format!("`{name}`"))
+            .join(", ")
+    }
+}
+
+#[derive(Error, Debug, Diagnostic)]
+#[error("Unexpected value")]
+#[diagnostic(
+    help("Expected one of these values: {}", self.print_expected_values()),
+    severity(Error)
+)]
+pub struct UnexpectedValue {
+    #[source_code]
+    pub source_code: NamedSourceCode,
+    #[label("The unexpected value")]
+    pub value_name: SourceSpan,
+
+    pub expected_values: Vec<&'static str>,
+}
+
+impl UnexpectedValue {
+    fn print_expected_values(&self) -> String {
+        self.expected_values
+            .iter()
+            .map(|name| format!("`{name}`"))
+            .join(", ")
+    }
+}
+
+#[derive(Error, Debug, Diagnostic)]
+#[error("Missing entry")]
+#[diagnostic(
+    help(
+        "This node should have one or more of these entries: {}", self.print_expected_entries()
+    ),
+    severity(Error)
+)]
+pub struct MissingEntry {
+    #[source_code]
+    pub source_code: NamedSourceCode,
+    #[label("For this node")]
+    pub node_name: SourceSpan,
+
+    pub expected_entries: Vec<&'static str>,
+}
+impl MissingEntry {
+    fn print_expected_entries(&self) -> String {
+        self.expected_entries
+            .iter()
+            .map(|name| format!("`{name}`"))
+            .join(", ")
+    }
+}
+
+#[derive(Error, Debug, Diagnostic)]
+#[error("Duplicate node")]
+#[diagnostic(
+    help("This type of node can only appear once. Remove the second occurance"),
+    severity(Error)
+)]
+pub struct DuplicateNode {
+    #[source_code]
+    pub source_code: NamedSourceCode,
+    #[label(primary, "The duplicate node")]
+    pub duplicate: SourceSpan,
+    #[label("The original node")]
+    pub original: SourceSpan,
+}
+
+#[derive(Error, Debug, Diagnostic)]
+#[error("Empty node")]
+#[diagnostic(
+    help("This type of node should have children to specify what it contains"),
+    severity(Warning)
+)]
+pub struct EmptyNode {
+    #[source_code]
+    pub source_code: NamedSourceCode,
+    #[label("The empty node")]
+    pub node: SourceSpan,
 }
