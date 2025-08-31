15773a65d3ad7199af100f18f9a2e30978de4c3c
Dion Dokter <diondokter@gmail.com>
2025-08-31 19:21:48 +0200

    Update convert_case

--- generation/src/mir/passes/device_name_is_pascal.rs
+++ generation/src/mir/passes/device_name_is_pascal.rs
@@ -4,8 +4,10 @@ use crate::mir::Device;
 
 pub fn run_pass(device: &mut Device) -> anyhow::Result<()> {
     let lenient_pascal_converter = convert_case::Converter::new()
-        .set_boundaries(&convert_case::Boundary::list_from("aA:AAa:_:-: :a1:A1:1A"))
-        .set_pattern(convert_case::Pattern::Capital);
+        .set_boundaries(&convert_case::Boundary::defaults_from(
+            "aA:AAa:_:-: :a1:A1:1A",
+        ))
+        .set_pattern(convert_case::pattern::capital);
     let converted_driver_name = lenient_pascal_converter.convert(device.name.as_ref().unwrap());
 
     ensure!(
