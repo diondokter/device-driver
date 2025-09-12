8c8c5aadfdd8d5c69425ecc056702681f89cd06f
Dion Dokter <diondokter@gmail.com>
2025-09-12 22:07:04 +0200

    Refactor ddc

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,140 +1,96 @@
 use clap::Parser;
 use device_driver_compiler::reporting::Diagnostics;
+use miette::{Context, IntoDiagnostic};
 use std::{
-    error::Error,
     io::Write,
     path::{Path, PathBuf},
+    process::ExitCode,
 };
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    /// Path to the manifest file.
-    /// The type of file is determined from the file extension.
-    #[arg(short = 'm', long = "manifest", value_name = "FILE")]
-    manifest_path: PathBuf,
+    /// Path to the input file.
+    input_path: PathBuf,
     /// Path to output location. Any existing file is overwritten. If not provided, the output is written to stdout.
     #[arg(short = 'o', long = "output", value_name = "FILE")]
     output_path: Option<PathBuf>,
     /// Type of generated output
-    #[arg(short = 'g', long = "gen-type", default_value = "rust")]
-    gen_type: GenType,
+    #[arg(short = 't', long = "target", default_value = "rust")]
+    target: Target,
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> ExitCode {
+    match run() {
+        Ok(exit) => exit,
+        Err(error) => {
+            eprintln!("{error:?}");
+            ExitCode::FAILURE
+        }
+    }
+}
+
+fn run() -> miette::Result<ExitCode> {
     let args = Args::parse();
 
     device_driver_compiler::reporting::set_miette_hook(true);
 
-    let extension = args
-        .manifest_path
-        .extension()
-        .map(|ext| ext.to_string_lossy())
-        .expect("Manifest file has no file extension");
+    let input_contents = std::fs::read_to_string(&args.input_path)
+        .into_diagnostic()
+        .wrap_err_with(|| {
+            format!(
+                "Trying to open input file at: {:?}",
+                args.input_path.display()
+            )
+        })?;
 
-    let manifest_contents = std::fs::read_to_string(&args.manifest_path).unwrap_or_else(|_| {
-        panic!(
-            "Trying to open manifest file at: \"{}\"",
-            args.manifest_path.display()
-        )
-    });
+    let (output, reports) = args.target.generate(&input_contents, &args.input_path);
 
-    let (output, reports) =
-        match args
-            .gen_type
-            .generate(&manifest_contents, &extension, &args.manifest_path)
-        {
-            Ok((output, reports)) => (Some(output), reports),
-            Err(reports) => (None, reports),
-        };
+    reports
+        .print_to(std::io::stderr().lock())
+        .into_diagnostic()?;
 
-    reports.print_to(std::io::stderr().lock()).unwrap();
+    if reports.has_error() {
+        return Ok(ExitCode::FAILURE);
+    }
 
-    if let Some(output) = output {
-        let output_writer: &mut dyn Write = match &args.output_path {
-            Some(path) => &mut std::fs::File::create(path).unwrap_or_else(|_| {
-                panic!(
-                    "Could not create the output file at: {}. Does its directory exist?",
+    let output_writer: &mut dyn Write = match &args.output_path {
+        Some(path) => &mut std::fs::File::create(path)
+            .into_diagnostic()
+            .wrap_err_with(|| {
+                format!(
+                    "Could not create the output file at: {:?}. Does its directory exist?",
                     path.display()
                 )
-            }),
-            None => &mut std::io::stdout().lock(),
-        };
+            })?,
+        None => &mut std::io::stdout().lock(),
+    };
 
-        let mut output_writer = std::io::BufWriter::new(output_writer);
-        output_writer
-            .write_all(output.as_bytes())
-            .expect("Could not write the output");
+    let mut output_writer = std::io::BufWriter::new(output_writer);
+    output_writer
+        .write_all(output.as_bytes())
+        .into_diagnostic()
+        .wrap_err_with(|| {
+            format!(
+                "Could not write output to {}",
+                args.output_path
+                    .map(|path| format!("{:?}", path.display()))
+                    .unwrap_or_else(|| "stdout".into())
+            )
+        })?;
 
-        if output.starts_with("::core::compile_error!") {
-            return Err(strip_compile_error(&output).into());
-        }
-
-        Ok(())
-    } else {
-        Err("Compilation ended with errors".to_string().into())
-    }
-}
-
-fn strip_compile_error(mut error: &str) -> &str {
-    error = error
-        .strip_prefix("::core::compile_error!(")
-        .unwrap_or(error)
-        .trim();
-    error = error.strip_prefix("\"").unwrap_or(error).trim();
-    error = error.strip_suffix(";").unwrap_or(error).trim();
-    error = error.strip_suffix(")").unwrap_or(error).trim();
-    error = error.strip_suffix(",").unwrap_or(error).trim();
-    error = error.strip_suffix("\"").unwrap_or(error).trim();
-
-    error
+    Ok(ExitCode::SUCCESS)
 }
 
 #[derive(clap::ValueEnum, Debug, Clone)]
-pub enum GenType {
+pub enum Target {
     Rust,
 }
 
-impl GenType {
-    pub fn generate(
-        &self,
-        manifest_contents: &str,
-        manifest_type: &str,
-        manifest_path: &Path,
-    ) -> Result<(String, Diagnostics), Diagnostics> {
+impl Target {
+    pub fn generate(&self, input_contents: &str, input_path: &Path) -> (String, Diagnostics) {
         match self {
-            GenType::Rust => match manifest_type {
-                "kdl" => Ok(device_driver_compiler::transform_kdl(
-                    manifest_contents,
-                    None,
-                    manifest_path,
-                )),
-                unknown => {
-                    panic!("Unknown manifest file extension: '{unknown}'. Only 'kdl' is allowed.")
-                }
-            },
+            Target::Rust => device_driver_compiler::transform_kdl(input_contents, None, input_path),
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn compile_error_stripping() {
-        assert_eq!(
-            strip_compile_error(
-                "::core::compile_error!(\"simple key expect ':' at byte 354 line 21 column 11\");"
-            ),
-            "simple key expect ':' at byte 354 line 21 column 11",
-        );
-
-        assert_eq!(
-            strip_compile_error(
-                "::core::compile_error!(\n    \"Parsing object `DOWNLOAD_POSTMORTEM`: Parsing error for 'fields_in': Parsing field 'LENGTH': Unexpected key: 'default'\",\n)\n"
-            ),
-            "Parsing object `DOWNLOAD_POSTMORTEM`: Parsing error for 'fields_in': Parsing field 'LENGTH': Unexpected key: 'default'"
-        );
     }
 }
