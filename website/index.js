879e515a93cf60ebf85d8761535b5484c9bdaf17
Dion Dokter <diondokter@gmail.com>
2025-09-14 14:56:54 +0200

    Restore functionality

--- website/index.js
+++ website/index.js
@@ -2,3 +2,18 @@ import * as wasm from './pkg';
 
 await wasm;
 
+const source = document.getElementById('source');
+const code_output = document.getElementById('code_output');
+const diagnostics_output = document.getElementById('diagnostics_output');
+
+source.addEventListener('change', run_compile);
+source.addEventListener('input', run_compile);
+
+run_compile();
+
+function run_compile() {
+    var output = wasm.compile(source.value);
+
+    code_output.innerText = output.code;
+    diagnostics_output.innerText = output.diagnostics;
+}
