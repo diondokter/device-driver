51316c3245639f1fff65b63c23c063fd00c58346
Dion Dokter <dev@diondokter.nl>
2026-04-21 18:22:26 +0200

    Turn off source maps in prod

--- website/webpack.config.ts
+++ website/webpack.config.ts
@@ -9,69 +9,73 @@ import MonacoWebpackPlugin from 'monaco-editor-webpack-plugin';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const config: webpack.Configuration = {
-    entry: {
-        home: './pages/home/index.ts',
-        playground: './pages/playground/index.ts',
-    },
-    output: {
-        path: path.resolve(__dirname, '..', 'dist', 'website'),
-        library: 'Website',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'pages/home/index.html',
-            chunks: ["home"],
-        }),
-        new HtmlWebpackPlugin({
-            template: 'pages/playground/index.html',
-            filename: 'playground/index.html',
-            chunks: ["playground"],
-        }),
-        // REMINDER: WasmPackPlugin is cooked. It works when wasm-pack is installed through cargo.
-        // Doesn't work with npm install (which the plugin will install if wasm-pack isn't found).
-        new WasmPackPlugin({
-            crateDirectory: path.resolve(__dirname, "../compiler/dd-wasm"),
-            watchDirectories: [
-                path.resolve(__dirname, "../compiler")
-            ],
-            outDir: path.resolve(__dirname, "pkg"),
-            outName: "device_driver_wasm",
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: 'assets', to: 'assets' },
-                { from: '../book/book', to: 'book' },
+const config = (env: any, argv: any): webpack.Configuration => {
+    const isProduction = argv.mode === 'production';
+
+    return {
+        entry: {
+            home: './pages/home/index.ts',
+            playground: './pages/playground/index.ts',
+        },
+        output: {
+            path: path.resolve(__dirname, '..', 'dist', 'website'),
+            library: 'Website',
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: 'pages/home/index.html',
+                chunks: ["home"],
+            }),
+            new HtmlWebpackPlugin({
+                template: 'pages/playground/index.html',
+                filename: 'playground/index.html',
+                chunks: ["playground"],
+            }),
+            // REMINDER: WasmPackPlugin is cooked. It works when wasm-pack is installed through cargo.
+            // Doesn't work with npm install (which the plugin will install if wasm-pack isn't found).
+            new WasmPackPlugin({
+                crateDirectory: path.resolve(__dirname, "../compiler/dd-wasm"),
+                watchDirectories: [
+                    path.resolve(__dirname, "../compiler")
+                ],
+                outDir: path.resolve(__dirname, "pkg"),
+                outName: "device_driver_wasm",
+            }),
+            new CopyWebpackPlugin({
+                patterns: [
+                    { from: 'assets', to: 'assets' },
+                    { from: '../book/book', to: 'book' },
+                ]
+            }),
+            new MonacoWebpackPlugin({
+                languages: ['rust']
+            }),
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader']
+                },
+                {
+                    test: /\.tsx?$/,
+                    use: "ts-loader",
+                    exclude: /node_modules/,
+                },
             ]
-        }),
-        new MonacoWebpackPlugin({
-            languages: ['rust']
-        }),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.tsx?$/,
-                use: "ts-loader",
-                exclude: /node_modules/,
-            },
-        ]
-    },
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"],
-    },
-    watchOptions: {
-        aggregateTimeout: 1000, // Delays the rebuild slightly to let WasmPack finish
-    },
-    mode: 'development',
-    devtool: 'inline-source-map',
-    experiments: {
-        asyncWebAssembly: true
-    }
+        },
+        resolve: {
+            extensions: [".tsx", ".ts", ".js"],
+        },
+        watchOptions: {
+            aggregateTimeout: 1000, // Delays the rebuild slightly to let WasmPack finish
+        },
+        mode: isProduction ? 'production' : 'development',
+        devtool: isProduction ? false : 'inline-source-map',
+        experiments: {
+            asyncWebAssembly: true
+        }
+    };
 };
 
 export default config;
