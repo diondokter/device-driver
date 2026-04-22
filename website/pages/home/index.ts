8c939507960bbff7f7b80a7c0eb310262cf10fc0
Dion Dokter <dev@diondokter.nl>
2026-04-22 22:27:07 +0200

    Copy the hljs themes and use media queries to add light theme

--- website/pages/home/index.ts
+++ website/pages/home/index.ts
@@ -1,7 +1,6 @@
 import hljs from 'highlight.js/lib/core';
 import { ddslLanguage } from './ddsl.highlight.js';
 import defineRust from 'highlight.js/lib/languages/rust';
-import 'highlight.js/styles/github-dark-dimmed.min.css';
 
 hljs.registerLanguage('ddsl', ddslLanguage);
 hljs.registerLanguage('rust', defineRust);
