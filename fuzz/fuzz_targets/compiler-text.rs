1a571960d4d34b5d6c2de86120e3bb03f1c0228e
Dion Dokter <dev@diondokter.nl>
2026-05-08 22:34:23 +0200

    Don't change the device original name to prevent later panics

--- fuzz/fuzz_targets/compiler-text.rs
+++ fuzz/fuzz_targets/compiler-text.rs
@@ -5,5 +5,8 @@ use libfuzzer_sys::fuzz_target;
 
 // Goal: Don't have panics or ICE's
 fuzz_target!(|source: &str| {
-    device_driver_core::compile(source, Target::Rust, Target::Rust.get_compile_options()).unwrap();
+    if let Err(e) = device_driver_core::compile(source, Target::Rust, Target::Rust.get_compile_options()) {
+        println!("======================\n{e:#}\n======================");
+        panic!();
+    }
 });
