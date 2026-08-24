f224f1dfc6b8dd969d6fd34fc041d2cd6efcf35d
Dion Dokter <dev@diondokter.nl>
2026-08-24 10:35:52 +0200

    Make the output more reproducible by not including the rustc version in the preamble

--- compiler/dd-core/build.rs
+++ compiler/dd-core/build.rs
@@ -1,27 +1,10 @@
 use std::process;
 
 fn main() {
-    let target = std::env::var("TARGET").unwrap();
-
-    println!("cargo:rustc-env=BUILDRS_TARGET={target}");
-    println!(
-        "cargo:rustc-env=BUILDRS_RUSTC={}",
-        get_rustc_version().as_deref().unwrap_or("unknown")
-    );
     println!(
         "cargo:rustc-env=BUILDRS_GIT_SHA={}",
         get_git_sha().as_deref().unwrap_or("unknown")
     );
-}
-
-fn get_rustc_version() -> Option<String> {
-    let rustc_executable = std::env::var("RUSTC").unwrap();
-
-    let output = process::Command::new(rustc_executable)
-        .arg("-V")
-        .output()
-        .ok()?;
-    Some(String::from_utf8(output.stdout).unwrap())
 }
 
 fn get_git_sha() -> Option<String> {
